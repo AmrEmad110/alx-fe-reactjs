11e0de9a7e4f1c8f3c42fd6281f83920db064cd5
Amr Emad <amreemad11@gmail.com>
2025-09-05 05:50:57 +0300

    commit

--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -1,5 +1,11 @@
 import React from 'react' ;
 
-function Userprofile = (props) => {
-
+export default function UserProfile(props) {
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <p>Age: {props.age}</p>
+      <p>Bio: {props.bio}</p>
+    </div>
+  );
 }
