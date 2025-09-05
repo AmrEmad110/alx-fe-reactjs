c5a9b96c438eec77bf0764f69f82cb6fc540e034
Amr Emad <amreemad11@gmail.com>
2025-09-05 03:44:26 +0300

    commit

---
+++ my-app/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';   // استيراد React
+
+function Footer() {
+  return (
+    <footer>
+      <p>© 2023 City Lovers</p>  {/* حقوق النشر */}
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ my-app/src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';   // استيراد React
+
+function MainContent() {
+  return (
+    <main>
+      <p>I love to visit New York, Paris, and Tokyo.</p>  {/* فقرة */}
+    </main>
+  );
+}
+
+export default MainContent;
