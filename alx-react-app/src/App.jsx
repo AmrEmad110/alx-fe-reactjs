a9125ecd598e3dbf93e6e75bb6fe880b97966dbe
Amr Emad <amreemad11@gmail.com>
2025-09-05 03:51:22 +0300

    commit

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,7 +3,7 @@
 import React from 'react';                            // استيراد React (للوضوح)
 import './App.css';                                   // استيراد ملف التنسيقات لو موجود
 import WelcomeMessage from './components/WelcomeMessage'; // استيراد المكوّن اللي عملناه
-import Header from './components/Header'; // استيراد المكوّن اللي عملناه
+// import Header from './components/Header'; // استيراد المكوّن اللي عملناه
 
 function App() {                                      // تعريف المكوّن الرئيسي App
   return (
