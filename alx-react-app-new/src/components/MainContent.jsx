e895e92e694ec5df371abb489b0447940cb28ef2
Amr Emad <amreemad11@gmail.com>
2025-09-12 01:38:36 +0300

    commit

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -3,8 +3,7 @@ function MainContent() {
     <main style={{ backgroundColor: '#f4f4f4', padding: '20px', minHeight: '200px' }}>
       <h2 style={{ textAlign: 'center', color: 'darkred' }}>Welcome to My Page</h2>
       <p style={{ textAlign: 'center' }}>
-        Here you can find information about my favorite cities and people I admire.
-      </p>
+I love to visit New York, Paris, and Tokyo.      </p>
     </main>
   );
 }
