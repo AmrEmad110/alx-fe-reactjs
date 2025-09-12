b3993c24f3632d7040a165ced8489c306853ee12
Amr Emad <amreemad11@gmail.com>
2025-09-12 03:20:02 +0300

    commit

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -2,6 +2,8 @@ import Header from './components/Header';
 import UserProfile from './components/UserProfile';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import Counter from "./components/Counter";
+
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Header />
       <UserProfile name="Amr Emad" age="22" bio="Frontend developer passionate about React and web development." />
       <MainContent />
+      <h1 style={{ textAlign: 'center', fontSize: '24px' }}>Simple Counter App</h1>
+      <Counter />
       <Footer />
     </div>
   );
