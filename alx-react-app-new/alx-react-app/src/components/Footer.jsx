3a8cc57570977df91c4b2132dc03a08ce89421a2
Amr Emad <149977409+AmrEmad110@users.noreply.github.com>
2025-09-22 03:14:01 +0300

    Delete alx-react-app-new/alx-react-app directory

--- alx-react-app-new/alx-react-app/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer style={{ backgroundColor: '#333', color: 'white', textAlign: 'center', padding: '10px' }}>
-      <p>© 2025 My Favorite Cities App</p>
-    </footer>
-  );
-}
-
-export default Footer;
