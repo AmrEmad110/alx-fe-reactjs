1b3a1ad1a506da0276a1ddc2abe3b2c13de51008
Amr Emad <amreemad11@gmail.com>
2025-10-12 04:53:26 +0300

    commit

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,9 +1,24 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter,  Routes, Route, Link } from "react-router-dom";
 import Home from "./Home";
 import About from "./About";
 
 export default function App() {
   return (
+    <BrowserRouter>
+      <div>
+        <nav style={{ marginBottom: "20px" }}>
+          <Link to="/">Home</Link> | <Link to="/about">About</Link>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
     <div>
       <nav style={{ marginBottom: "20px" }}>
         <Link to="/">Home</Link> | <Link to="/about">About</Link>
